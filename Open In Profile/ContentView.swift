f91311581b1679e2464b6c4ec2036088a470fcca
hcancelik <celik@me.com>
2020-05-09 21:17:04 +0300

    Initial Commit

---
+++ Open In Profile/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Open In Profile
+//
+//  Created by H. Can Celik on 5/9/20.
+//  Copyright © 2020 H. Can Celik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
